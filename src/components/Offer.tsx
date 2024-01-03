7bc2ce49ab5984db019b9c571937c66b8785efbc
Abderraouf Drine <abderraoufdrine@gmail.com>
2024-01-03 00:29:10 -0500

    Update Offer.tsx

--- src/components/Offer.tsx
+++ src/components/Offer.tsx
@@ -9,7 +9,7 @@ const Offer = () => {
       <div className="flex-1 flex flex-col justify-center items-center text-center gap-8 p-6">
         <h1 className="text-white text-5xl font-bold xl:text-6xl">Delicious Burger & French Fry</h1>
         <p className="text-white xl:text-xl">
-          Progressively simplify effective e-toilers and process-centric methods
+          Progressively simplify effective e-toilers
           of empowerment. Quickly pontificate parallel.
         </p>
         <CountDown/>
